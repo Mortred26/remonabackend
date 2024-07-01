ca04ad2116603f47d289cd7f4f0245fffb51dcd2
Mortred26 <umarnurxodjayev29@gmail.com>
2024-07-01 21:22:43 +0500

    fix refresh token

--- Middleware/refreshMiddleware.js
+++ Middleware/refreshMiddleware.js
@@ -1,31 +1,32 @@
 const jwt = require('jsonwebtoken');
-const { User } = require('../models/user');
 const { Admin } = require('../models/admin');
+const { User } = require('../models/user');
 
 module.exports = async function(req, res, next) {
-  const refreshToken = req.header('x-refresh-token');
+  const authHeader = req.headers['authorization'];
+  const refreshToken = authHeader && authHeader.split(' ')[1]; // Bearer <refreshToken>
+
   if (!refreshToken) return res.status(401).send('Access denied. No refresh token provided.');
 
   try {
-    // Verify the refresh token
     const decoded = jwt.verify(refreshToken, process.env.JWT_REFRESH_KEY);
-    
-    // Check if the decoded token is for a user or an admin
-    let user = await User.findById(decoded._id);
-    if (!user) {
+
+    let user;
+    if (decoded.role === 'admin') {
       user = await Admin.findById(decoded._id);
+    } else {
+      user = await User.findById(decoded._id);
     }
 
-    if (!user) {
+    if (!user || decoded.role !== user.role) {
       throw new Error('Invalid refresh token.');
     }
 
-    req.user = user; // Attach the user or admin object to the request
-    next(); // Pass control to the next middleware function
+    req.user = user;
+    next();
   } catch (ex) {
     console.error('Token verification error:', ex);
 
-    // Handle specific errors
     if (ex.name === 'TokenExpiredError') {
       return res.status(401).send('Refresh token expired. Please log in again.');
     } else if (ex.name === 'JsonWebTokenError' || ex.message === 'Invalid token.') {
